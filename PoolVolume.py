2dee52afd162c68f325dde5dda0c53aedcf3e893
Phillip McClure <23pmcclure@athenian.org>
2020-02-27 15:46:32 -0800

    Add PoolVolume

---
+++ PoolVolume.py
@@ -0,0 +1,192 @@
+import math as m
+
+mm_cm = 10
+cm_in = 2.54
+in_ft = 12
+ft_yd = 3
+yd_m = 1.09361
+m_km = 1000
+global great_conversion
+global less_conversion
+global conversion_value
+global conversion_type1
+global conversion_type2
+global conversion_multiplier
+global height
+global width
+global length
+
+
+# This asks for the dimensions for a 3D shape
+def dimension_questions():
+    global height
+    global length
+    global width
+    height = float(input("Insert height: "))
+    width = float(input("Insert width: "))
+    length = float(input("Insert length: "))
+
+
+# This converts the value to the right unit
+def conversion():
+    global conversion_type1
+    global conversion_type2
+    global conversion_value
+    if conversion_type1 == conversion_type2:
+        print("You already have the right unit!")
+    else:
+        while conversion_type1 > conversion_type2:
+            set_conversions()
+            conversion_value = conversion_value * (less_conversion ** conversion_multiplier)
+            conversion_type1 = conversion_type1 - 1
+        while conversion_type1 < conversion_type2:
+            set_conversions()
+            conversion_value = conversion_value / (great_conversion ** conversion_multiplier)
+            conversion_type1 = conversion_type1 - 1
+        if conversion_type1 == conversion_type2:
+            print("Your converted value is " + str(conversion_value))
+
+
+# This assigns the conversion variable to the correct value
+def set_conversions():
+    global great_conversion
+    global less_conversion
+    if conversion_type1 == 1:
+        great_conversion = mm_cm
+    elif conversion_type1 == 2:
+        less_conversion = mm_cm
+        great_conversion = cm_in
+    elif conversion_type1 == 3:
+        less_conversion = cm_in
+        great_conversion = in_ft
+    elif conversion_type1 == 4:
+        less_conversion = in_ft
+        great_conversion = ft_yd
+    elif conversion_type1 == 5:
+        less_conversion = ft_yd
+        great_conversion = yd_m
+    elif conversion_type1 == 6:
+        less_conversion = yd_m
+        great_conversion = m_km
+    elif conversion_type1 == 7:
+        less_conversion = m_km
+
+
+# This asks for conversion units
+def conversion_questions():
+    global conversion_type1
+    global conversion_type2
+    global conversion_value
+    print("If mm type 1")
+    print("If cm type 2")
+    print("If in type 3")
+    print("If ft type 4")
+    print("If yd type 5")
+    print("If m type 6")
+    print("If km type 7")
+    conversion_value = float(input("What number will you be converting: "))
+    conversion_type1 = int(input("What unit will you convert: "))
+    conversion_type2 = int(input("What unit will you be converting to: "))
+
+
+# This prints the final converted value
+def conversion_result():
+    global conversion_vlaue
+    if conversion_type1 == conversion_type2:
+        print("Your converted value is " + str(conversion_value))
+
+
+# This prints the results for 3D shapes
+def results():
+    print("Your volume is: " + str(volume))
+    print("Your surface area is: " + str(surface_area))
+
+
+first_choice = int(input("If finding measurements press 1. If converting press 2: "))
+if first_choice == 1:
+    print("Do you want to find the volume and surface area or the area and perimeter of a shape?")
+    print("If rectangular prism press 1.")
+    print("If right triangular prism press 2.")
+    print("If uneven triangular prism press 3.")
+    print("If 2D circle press 4.")
+    print("If 2D trapezoid press 5.")
+    print("If rectangle or square press 6.")
+    print("If sphere press 7.")
+    start = int(input("Enter answer here: "))
+    # This finds the measurements for a rectangular prism
+    if start == 1:
+        dimension_questions()
+        surface_area = ((height * width) * 2) + ((width * length) * 2) + ((height * length) * 2)
+        volume = height * length * width
+        results()
+    # This finds measurements for a right triangular prism
+    elif start == 2:
+        dimension_questions()
+        volume = (height * length * width) / 2
+        hypotenuse = (length ** 2) + (height ** 2)
+        hypotenuse_real = m.sqrt(hypotenuse)
+        surface_area = (length * width) + (height * width) + (hypotenuse_real * width) + (height * length)
+        results()
+    # This finds measurements for an uneven triangular prism
+    elif start == 3:
+        dimension_questions()
+        volume = (length * width * height) / 2
+        side_length1 = float(input("Enter length of side of triangle: "))
+        side_length2 = float(input("Enter other length of side of triangle: "))
+        surface_area = (length * width) + (height * width) + (side_length1 * length) + (side_length2 * length)
+        results()
+    # This finds measurements for a circle
+    elif start == 4:
+        diameter = float(input("What is your diameter: "))
+        surface_area = diameter * m.pi
+        volume = ((diameter / 2) ** 2) * m.pi
+        print("Your area is: " + str(volume))
+        print("Your perimeter is: " + str(surface_area))
+    # This finds measurements for a trapezoid
+    elif start == 5:
+        base1 = float(input("What is the base: "))
+        base2 = float(input("What is your other base: "))
+        height = float(input("What is your height: "))
+        if base1 >= base2:
+            base = base2
+            base3 = (base1 - base2) / 2
+        else:
+            base = base1
+            base3 = (base2 - base1) / 2
+        volume = (base3 + base) * height
+        print(str(volume))
+    #This finds measurements for a rectangle or square
+    elif start == 6:
+        height = float(input("What is your length: "))
+        length = float(input("What is your height: "))
+        volume = height * length
+        surface_area = (height + length) * 2
+        print("Your area is: " + str(volume))
+        print("Your surface area is: " + str(surface_area))
+    elif start == 7:
+        diameter = float(input("What is your diameter: "))
+        volume = (4 / 3) * m.pi * ((diameter / 2) ** 3)
+        surface_area = 4 * m.pi * ((diameter / 2) ** 2)
+    else:
+        print("Suit yourself. No fun for you!")
+elif first_choice == 2:
+    conversion_scale = int(input("If 3D object press 1. If 2D object press 2. If 1D object press 3: "))
+    # This finds the units for a 3D object
+    if conversion_scale == 1:
+        conversion_questions()
+        conversion_multiplier = 3
+        conversion()
+    # This finds the units for a 2D object
+    elif conversion_scale == 2:
+        conversion_questions()
+        conversion_multiplier = 2
+        conversion()
+    # This finds the units for a 1D object
+    elif conversion_scale == 3:
+        conversion_questions()
+        conversion_multiplier = 1
+        conversion()
+    else:
+        print("Suit yourself. No fun for you!")
+else:
+    print("Suit yourself. No fun for you!")
